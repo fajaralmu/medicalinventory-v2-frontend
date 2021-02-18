a179664973661c9382690a74490d32a40a2c1a97
Fajar AM <somabangsa@gmail.com>
2021-02-18 08:42:28 +0700

    Update Login page

--- src/component/pages/login/Login.tsx
+++ src/component/pages/login/Login.tsx
@@ -9,10 +9,10 @@ import './Login.css';
 import { performLogin } from '../../../redux/actionCreators';
 import Spinner from './../../loader/Spinner';
 class IState {
-    loading:boolean = false; username:string = ""; password: string = "";
+    loading: boolean = false; username: string = ""; password: string = "";
 }
-class Login extends BaseComponent{
-    state:IState = new IState();
+class Login extends BaseComponent {
+    state: IState = new IState();
     constructor(props: any) {
         super(props, false);
     }
@@ -20,16 +20,16 @@ class Login extends BaseComponent{
     endLoading = () => this.setState({ loading: false });
     login(e: FormEvent) {
         e.preventDefault();
-        this.props.performLogin(this.state.username,this.state.password, this);
+        this.props.performLogin(this.state.username, this.state.password, this);
     }
-    componentDidMount(){
+    componentDidMount() {
         document.title = "Login";
         if (this.isUserLoggedIn()) {
             this.props.history.push("/dashboard");
         }
     }
-    componentDidUpdate(){
-    
+    componentDidUpdate() {
+
         // console.debug("Login update");
         // console.debug("logged in : ", this.props.loginStatus);
         // console.debug("logged user : ", this.getLoggedUser());
@@ -37,24 +37,36 @@ class Login extends BaseComponent{
             this.props.history.push("/dashboard");
         }
     }
-    updateCredentialProperty = (e:ChangeEvent) => {
+    updateCredentialProperty = (e: ChangeEvent) => {
         const target = e.target as HTMLInputElement;
-        const name:string|null = target.getAttribute("name");
+        const name: string | null = target.getAttribute("name");
         if (null == name) return;
-        this.setState({[name]: target.value});
+        this.setState({ [name]: target.value });
     }
     render() {
         return (
-            <div id="Login">
+            <div id="Login" className="text-center">
+                 
+                <svg style={{marginTop:'20px'}} width="200" height="201" className="sloginicon">
+                    <path className="animated-svg house" fill="transparent" stroke="black" strokeWidth={3}
+                        d="M 100 0 L 0 100 L 15 100 L 15 200 L 185 200 
+                    L 185 100 L 200 100  Z"
+                    />
+                    <circle className="animated-svg circle" fill="transparent" stroke="blue" strokeWidth={3}
+                        cx={100} cy={122} r={68} />
+                    <path className="animated-svg plus" fill="transparent" stroke="blue" strokeWidth={3}
+                        d="M 41 111 L 88 111 L 88 64 L 113 64 
+                    L 113 111 L 158 111 L 158 135 L 113 135 
+                    L 113 181 L 88 181 L 88 135 L 41 135 Z"
+
+                    />
+
+                </svg>
                 <form name='login' onSubmit={(e) => { this.login(e) }}
                     method='POST' className="form-signin">
-                    <div className="text-center">
-                        <h2><i className="fas fa-user-circle"></i></h2>
-                        <h1 className="h3 mb-3 font-weight-normal">Masuk</h1>
-                    </div>
-                    <UsernameField value={this.state.username} onChange={this.updateCredentialProperty}/>
-                    <PasswordField value={this.state.password} onChange={this.updateCredentialProperty}/>
-                    {this.state.loading ? <Spinner/>:<button className="btn btn-lg btn-success btn-block" type="submit">Masuk</button>}
+                    <UsernameField value={this.state.username} onChange={this.updateCredentialProperty} />
+                    <PasswordField value={this.state.password} onChange={this.updateCredentialProperty} />
+                    {this.state.loading ? <Spinner /> : <button className="btn btn-lg btn-dark btn-block" type="submit">Masuk</button>}
                     <input name="transport_type" type="hidden" value="rest" />
                 </form>
             </div>
@@ -62,17 +74,17 @@ class Login extends BaseComponent{
     }
 
 }
-const PasswordField = ({value, onChange}) => {
+const PasswordField = ({ value, onChange }) => {
     return <Fragment>
         <label className="sr-only">Kata Sandi</label>
-        <input name="password" value={value} onChange={onChange} type="password" id="inputPassword" className="form-control"
+        <input name="password" value={value} onChange={onChange} type="password" id="inputPassword" className="form-control border border-dark"
             placeholder="Password" required />
     </Fragment>
 }
-const UsernameField = ({value, onChange}) => {
+const UsernameField = ({ value, onChange }) => {
     return (<Fragment>
         <label className="sr-only">Nama Pengguna</label>
-        <input name="username" value={value} onChange={onChange} type="text" id="username" className="form-control"
+        <input name="username" value={value} onChange={onChange} type="text" id="username" className="form-control border border-dark"
             placeholder="Username" required autoFocus />
     </Fragment>)
 }
